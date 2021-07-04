e183980a63397c97fa83756d640aab77660ac5c3
Jan Bodnar <jan.bodnar@gmail.com>
2021-07-04 22:44:42 +0200

    Add files via upload

---
+++ cars_postgres.sql
@@ -0,0 +1,9 @@
+INSERT INTO cars(Name, Price) VALUES('Audi',52642);
+INSERT INTO cars(Name, Price) VALUES('Mercedes',57127);
+INSERT INTO cars(Name, Price) VALUES('Skoda',9000);
+INSERT INTO cars(Name, Price) VALUES('Volvo',29000);
+INSERT INTO cars(Name, Price) VALUES('Bentley',350000);
+INSERT INTO cars(Name, Price) VALUES('Citroen',21000);
+INSERT INTO cars(Name, Price) VALUES('Hummer',41400);
+INSERT INTO cars(Name, Price) VALUES('Volkswagen',21600);
+
