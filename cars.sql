3f5458f86770acb5b08e2192ce0d6ddba8799dab
Jan Bodnar <jan.bodnar@gmail.com>
2021-09-30 14:15:30 +0200

    Update cars.sql

--- cars.sql
+++ cars.sql
@@ -1,12 +1,12 @@
--- SQL for the Cars table
+-- SQL for the cars table
 
-CREATE TABLE CARS(ID BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
+CREATE TABLE cars(ID BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
     (START WITH 1, INCREMENT BY 1), NAME VARCHAR(30), PRICE INT);
-INSERT INTO CARS(Name, Price) VALUES('Audi', 52642);
-INSERT INTO CARS(Name, Price) VALUES('Mercedes', 57127);
-INSERT INTO CARS(Name, Price) VALUES('Skoda', 9000);
-INSERT INTO CARS(Name, Price) VALUES('Volvo', 29000);
-INSERT INTO CARS(Name, Price) VALUES('Bentley', 350000);
-INSERT INTO CARS(Name, Price) VALUES('Citroen', 21000);
-INSERT INTO CARS(Name, Price) VALUES('Hummer', 41400);
-INSERT INTO CARS(Name, Price) VALUES('Volkswagen', 21600);
+INSERT INTO cars(name, price) VALUES('Audi', 52642);
+INSERT INTO cars(name, price) VALUES('Mercedes', 57127);
+INSERT INTO cars(name, price) VALUES('Skoda', 9000);
+INSERT INTO cars(name, price) VALUES('Volvo', 29000);
+INSERT INTO cars(name, price) VALUES('Bentley', 350000);
+INSERT INTO cars(name, price) VALUES('Citroen', 21000);
+INSERT INTO cars(name, price) VALUES('Hummer', 41400);
+INSERT INTO cars(name, price) VALUES('Volkswagen', 21600);
