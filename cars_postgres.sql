8bb852aadeac4197e2f6d2edfe9b64b234270881
Jan Bodnar <jan.bodnar@gmail.com>
2021-09-30 14:20:15 +0200

    Update cars_postgres.sql

--- cars_postgres.sql
+++ cars_postgres.sql
@@ -1,9 +1,9 @@
-INSERT INTO cars(Name, Price) VALUES('Audi',52642);
-INSERT INTO cars(Name, Price) VALUES('Mercedes',57127);
-INSERT INTO cars(Name, Price) VALUES('Skoda',9000);
-INSERT INTO cars(Name, Price) VALUES('Volvo',29000);
-INSERT INTO cars(Name, Price) VALUES('Bentley',350000);
-INSERT INTO cars(Name, Price) VALUES('Citroen',21000);
-INSERT INTO cars(Name, Price) VALUES('Hummer',41400);
-INSERT INTO cars(Name, Price) VALUES('Volkswagen',21600);
-
+CREATE TABLE cars(id serial PRIMARY KEY, name VARCHAR(255), price INT);
+INSERT INTO cars(name, price) VALUES('Audi', 52642);
+INSERT INTO cars(name, price) VALUES('Mercedes', 57127);
+INSERT INTO cars(name, price) VALUES('Skoda', 9000);
+INSERT INTO cars(name, price) VALUES('Volvo', 29000);
+INSERT INTO cars(name, price) VALUES('Bentley', 350000);
+INSERT INTO cars(name, price) VALUES('Citroen', 21000);
+INSERT INTO cars(name, price) VALUES('Hummer', 41400);
+INSERT INTO cars(name, price) VALUES('Volkswagen', 21600);
